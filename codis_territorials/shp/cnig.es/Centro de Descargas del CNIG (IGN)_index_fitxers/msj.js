da89e49b51de3b0c92f8516b33415a4cd9ec9a55
Xavier de Pedro <xavi@confluencia.net>
2020-06-01 11:34:04 +0200

    afegides carpetes codis territorials geo, cityos, shp, i vodafone , ignornat els .zip de shp (que pesen més de 50Mb, i on calgui, ja els posaré via owncloud o altres vies)

---
+++ codis_territorials/shp/cnig.es/Centro de Descargas del CNIG (IGN)_index_fitxers/msj.js
@@ -0,0 +1,61 @@
+var ajaxErrorGral={"ES":"No se ha podido realizar la conexión con el servidor...<br> Inténtelo más tarde.","EN":"Could not connect to server...<br> Try again later.","FR":"No se ha podido realizar la conexión con el servidor...<br> Inténtelo más tarde."};
+var areaOblig={"ES":"Seleccione al menos un área temática.","EN":"Seleccione al menos un área temática.","FR":"Seleccione al menos un área temática."};
+var cancelarTramitacion={"ES":"¿Está seguro de cancelar la tramitación?","EN":"¿Esta seguro de cancelar la tramitación?","FR":"¿Esta seguro de cancelar la tramitación?"};
+var descUsoOblig={"ES":"La descripción del uso es obligatoria.","EN":"La descripción del uso es obligatoria.","FR":"La descripción del uso es obligatoria."};
+var direccion={"ES":"Dirección","EN":"Address","FR":"Direccion"};
+var escala={"ES":"Escala","EN":"Scale","FR":"Échelle"};
+var infoHojaVacia={"ES":"Introduzca un valor para el número de hoja","EN":"Enter a value for the sheet number","FR":"Introduzca un valor para el número de hoja"};
+var infoCoordXY={"ES":"Introduzca un valor para la coordenada #coord#","EN":"Enter a value for the coordinate #coord#","FR":"Introduzca un valor para la coordenada #coord#"};
+var infoLon={"ES":"Introduzca un valor para la longitud","EN":"Enter a value for longitude","FR":"Introduzca un valor para la longitud"};
+var infoLat={"ES":"Introduzca un valor para la latitud","EN":"Enter a value for latitude","FR":"Introduzca un valor para la latitud"};
+var infoLimitHoja={"ES":"El número de hoja debe estar entre 1 y 1111.","EN":"Sheet number must be between 1 and 1111.","FR":"El número de hoja debe estar entre 1 y 1111."};
+var infoRC={"ES":"14 primeros dígitos de la referencia catastral.","EN":"14 first digits of the cadastral reference.","FR":"14 primeros dígitos de la referencia catastral."};
+var paisOblig={"ES":"El país es obligatorio","EN":"Country is required","FR":"El país es obligatorio"};
+var perfilOblig={"ES":"El perfil es obligatorio.","EN":"El perfil es obligatorio.","FR":"El perfil es obligatorio."};
+var procesando={"ES":"Procesando petición...<br> Espere por favor.","EN":"Processing request... please wait.","FR":"Procesando peticion...\n Espere por favor."};
+var comOblig={"ES":"La Comunidad Autónoma es obligatoria.","EN":"Comunidad Autónoma is required.","FR":"La Comunidad Autónoma es obligatoria."};
+var munOblig={"ES":"El municipio es obligatorio.","EN":"Village is required.","FR":"El municipio es obligatorio."};
+var provOblig={"ES":"La provincia es obligatoria.","EN":"Province is required.","FR":"La provincia es obligatoria."};
+var selectDivAdm={"ES":"Seleccione alguna opción de división administrativa.","EN":"Select some administrative division option.","FR":"Seleccione alguna opción de división administrativa."};
+var selectHojaDivAdm={"ES":"Seleccione hoja o división administrativa.","EN":"Select sheet or administrative division.","FR":"Seleccione hoja o división administrativa."};
+var selectProd={"ES":"Seleccione producto.","EN":"Select product.","FR":"Seleccione producto."};
+var selectSerie={"ES":"Seleccione al menos una serie de la lista.","EN":"Select at least one item from the list.","FR":"Seleccione al menos una serie de la lista."};
+var selectOpBusc={"ES":"Seleccione alguna opción de búsqueda.","EN":"Select a search option.","FR":"Seleccione alguna opcion de busqueda."};
+var topoDirCp={"ES":"Buscar por topónimo, dirección o código postal","EN":"Search by place name, address or zip code","FR":"Buscar por topónimo, dirección o código postal"};
+var usoDescarga={"ES":"introduzca el uso al que va a ser destinada la descarga","EN":"Enter the use to which the download will be destined","FR":"introduzca el uso al que va a ser destinada la descarga"};
+var waiting={"ES":"Cargando datos...<br> Espere por favor.","EN":"Loading data... please wait.","FR":"Cargando datos...<br> Espere por favor."};
+
+
+//david añade el mensaje para el limite de archivos en la cesta.
+var limiteArchivos={"ES":"Esta operación supera el límite de ficheros permitidos en la Cesta de descargas (40.000).</br>Por favor, elimine ficheros de la cesta o realice la descarga de los ya seleccionados.","EN":"This action exceed the amount of files allowed within the Download cart (40 000).</br>Please, delete files from the cart or download the already selected files.","FR":"Cette action dépasse le nombre de fichiers autorisés dans le panier de téléchargement (40 000). </br>Supprimez les fichiers du panier ou téléchargez les fichiers déjà sélectionnés."};
+//david añade el mensaje para el limite de archivos en la cesta.
+
+var limiteArea={"ES":"El limite para la búsqueda por polígono son 95.000 km2","EN":"The limit for polygon search is 95.000 km2","FR":"El limite para la búsqueda por polígono son 95.000 km2"};
+var limiteAreaBBOX={"ES":"El limite para la búsqueda por BBOX son 95.000 km2","EN":"El limite para la búsqueda por BBOX son 95.000 km2","FR":"El limite para la búsqueda por BBOX son 95.000 km2"};
+var limiteAreaShape={"ES":"El limite para el shape son 95.000 km2","EN":"The limit for the shape file is 95.000 km2","FR":"El limite para el shape son 95.000 km2"};
+var formatosValidos={"ES":"Formatos permitidos:ZIP(shape & dbf), KML y GPX","EN":"Allowed formats:ZIP(shape & dbf), KML y GPX","FR":"Formatos permitidos:ZIP(shape & dbf), KML y GPX"};
+var kmlError={"ES":"KML mal formado","EN":"Wrong format for KML file","FR":"KML mal formado"};
+var gpxError={"ES":"GPX mal formado","EN":"Wrong format for GPX file","FR":"GPX mal formado"};
+var shpInfo={"ES":"El fichero Shape solo puede tener un tipo de geometría","EN":"El shape solo puede tener un tipo de geometría","FR":"El shape solo puede tener un tipo de geometría"};
+var kmlInfo={"ES":"El fichero KML solo puede tener un tipo de geometría","EN":"El fichero KML solo puede tener un tipo de geometría","FR":"El fichero KML solo puede tener un tipo de geometría"};
+var gpxInfo={"ES":"El fichero GPX solo puede tener un tipo de geometría","EN":"El fichero GPX solo puede tener un tipo de geometría","FR":"El fichero GPX solo puede tener un tipo de geometría"};
+
+var cargarFicheroSKG={"ES":"Cargue un fichero zip(shp & dbf), kml o gpx","EN":"Upload a zip(shp & dbf), kml or gpx file","FR":"Cargue un fichero zip(shp & dbf), kml o gpx"};
+
+var nombreObligatorio={"ES":"El nombre es obligatorio","EN":"Name is required","FR":"El nombre es obligatorio"};
+var nombreError={"ES":"Nombre no válido","EN":"Invalid name","FR":"Nombre no válido"};
+var mailObligatorio={"ES":"El mail es obligatorio","EN":"Mail is required","FR":"El mail es obligatorio"};
+var mailError={"ES":"Mail no válido","EN":"Invalid mail","FR":"Mail no válido"};
+var comentObligatorio={"ES":"Introduzca su consulta","EN":"Enter your query","FR":"Introduzca su consulta"};
+var asuntoObligatorio={"ES":"Introduzca un asunto","EN":"Enter a subject","FR":"Introduzca un asunto"};
+var sendConsultaOk={"ES":"Su consulta se ha enviado correctamente.","EN":"Your query has been sent successfully.","FR":"Su consulta se ha enviado correctamente."};
+var sendConsultaKo={"ES":"No se ha podido enviar su consulta, por favor, inténtelo más tarde.","EN":"Your query could not be sent, please try again later.","FR":"No se ha podido enviar su consulta, por favor, inténtelo más tarde."};
+
+var noCoordsReferCatas={"ES":"No hay unas coordenadas válidas para la referencia catastral introducida.","EN":"There are no valid coordinates for the cadastral reference entered.","FR":"No hay unas coordenadas válidas para la referencia catastral introducida."};
+var coordenadaNoValida={"ES":"Coordenada no válida.","EN":"Invalid coordinate.","FR":"Coordenada no válida."};
+var faltanParams={"ES":"Faltan parámetros en la petición.","EN":"Missing parameters in request.","FR":"Faltan parámetros en la petición."};
+var peticionIncorrecta={"ES":"Petición incorrecta.","EN":"Wrong request.","FR":"Petición incorrecta."};
+var errLon={"ES":"Longitud en grados decimales, mayor que cero y máximo 8 decimales (seleccione E:este O:oeste).<br>p.e: 3.71024042 O","EN":"Longitude in decimal degrees, greater than zero and maximum 8 decimals (select E:East O:West).<br>i.e: 3.71024042 O","FR":"Longitud en grados decimales, mayor que cero y máximo 8 decimales (seleccione E:este O:oeste)."};
+var errLat={"ES":"Latitud en grados decimales, mayor que cero y máximo 8 decimales.","EN":"Latitude in decimal degrees, greater than zero and maximum 8 decimals.","FR":"Latitud en grados decimales, mayor que cero y máximo 8 decimales."};
+var errUtm={"ES":"Valor UTM no permitido.","EN":"UTM value not allowed.","FR":"Valor UTM no permitido."};
+
